a445fbb0bff0e2a118c048d5198d2391e3e658ab
Leeson <leeson.leeson@outlook.com>
2021-06-04 00:26:59 +0800

    增加了login和sugar

--- MidTerm/urls.py
+++ MidTerm/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('btForum.urls'))
+    path('',include('btForum.urls')),
+    path('index/', TemplateView.as_view(template_name='static/sugar/sugar.html')),
+    path('login1/', TemplateView.as_view(template_name='static/login/login.html'))
 ]
